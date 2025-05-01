3845b4a68a9d131a25a7a976a455d69a27a08a18
OlenaKorbut83 <olenakorbut83@gmail.com>
2025-05-01 13:10:03 +0300

    bug fixed

--- src/middleware/isValideId.js
+++ src/middleware/isValideId.js
@@ -5,7 +5,7 @@ import createHttpError from 'http-errors';
 export const isValideId = (req, res, next) => {
   const { id } = req.params;
   if (!isValidObjectId(id)) {
-    return next(createHttpError(404, `${id} is not valide id`));
+    return next(createHttpError(400, `${id} is not valide id`));
   }
   next();
 };
